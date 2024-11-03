f28948f34732d87b0f5535a5e6dad452ea75f8e1
yahdinaahsya <yahdinakyeopta74@gmail.com>
2024-11-03 17:07:31 +0700

    create : layout welcome page & home page

---
+++ lib/pages/list_bus/list_bus.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ListBusPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ListBusPageState();
+  }
+}
+
+class _ListBusPageState extends State<ListBusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text('Nama Halte'),
+        titleTextStyle: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(left: 16, right: 16),
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ListBusPage()),
+                        );
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        color: Theme.of(context).colorScheme.secondary,
+                        child: ListTile(
+                          leading: SvgPicture.asset('images/ic_bus_stop.svg', width: 40, height: 40,),
+                          title: Text('Bus $index'),
+                          titleTextStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          trailing: Text('18.00 WIB'),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+      ),
+    );
+  }
+}
--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_trans_kutaraja/pages/Home/home.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -6,12 +7,53 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome to Flutter'),
-      ),
-      body: const Center(
-        child: Text('Hello, World!'),
-      ),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    child: const Text(
+                      'Selamat datang di MyTransKutaraja',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    margin: EdgeInsets.only(bottom: 10),
+                  ),
+                  const Text(
+                    'Nikmati kemudahan akses informasi keberangkatan bus secara real-time!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Image.asset('images/bus_welcome.png'),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll<Color>(
+                    Theme.of(context).primaryColor),
+              ),
+              child: const Text('Ayo, lacak bus di sini!',
+                  style: TextStyle(fontSize: 14, color: Colors.white)),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+            ),
+          ]),
     );
   }
 }
